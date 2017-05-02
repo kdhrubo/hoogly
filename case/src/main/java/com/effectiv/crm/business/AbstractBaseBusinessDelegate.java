26ee08d2f8fb8dd87fe3257a4bbd3448e3d3903b
Dhrubo <dhrubo.kayal@gmail.com>
2017-05-01 19:12:53 -0500

    First commit

---
+++ case/src/main/java/com/effectiv/crm/business/AbstractBaseBusinessDelegate.java
@@ -0,0 +1,93 @@
+package com.effectiv.crm.business;
+
+import java.io.Serializable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.effectiv.crm.domain.BaseEntity;
+import com.effectiv.crm.exception.BusinessException;
+import com.effectiv.crm.repository.BaseRepository;
+import com.effectiv.crm.web.SearchRequest;
+
+import jodd.bean.BeanUtil;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class AbstractBaseBusinessDelegate<T extends BaseEntity, Id extends Serializable> {
+	@Getter
+	private BaseRepository<T, Id> repository;
+
+	public AbstractBaseBusinessDelegate(BaseRepository<T, Id> repository) {
+		this.repository = repository;
+	}
+
+	@Transactional(readOnly = true)
+	public T findOne(Id id) {
+		return repository.findOne(id);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<T> findAll(SearchRequest searchRequest, Pageable pageable) {
+		return repository.findAll(searchRequest, pageable);
+	}
+
+	
+
+	@Transactional
+	public T save(T t) {
+		log.info("saving ---> " + t);
+		return repository.save(t);
+	}
+
+	@Transactional
+	public void delete(Id id) {
+
+		T t = findOne(id);
+
+		if (t == null) {
+			throw new BusinessException(
+					"Record with id = " + id + " not found. May be it is already deleted or purged. ");
+		}
+
+		try {
+
+			BeanUtil.silent.setProperty(t, "deleted", true);
+		} catch (Exception e) {
+			log.error("Error while setting deleted property in restore - {}", e);
+			throw new BusinessException("Entity cannot be deleted (soft delete) - " + e.getLocalizedMessage()
+					+ ". Check if deleted field is present.");
+		}
+		repository.save(t);
+	}
+
+	@Transactional
+	public void purge(Id id) {
+		repository.delete(id);
+	}
+
+	
+
+	@Transactional
+	public void restore(Id id) {
+
+		T t = findOne(id);
+
+		if (t == null) {
+			throw new BusinessException(
+					"Record with id = " + id + " not found. May be it is already deleted or purged. ");
+		}
+
+		try {
+
+			BeanUtil.silent.setProperty(t, "deleted", false);
+		} catch (Exception e) {
+			log.error("Error while setting deleted property in restore - {}", e);
+			throw new BusinessException("Entity cannot be deleted (soft delete) - " + e.getLocalizedMessage()
+					+ ". Check if deleted field is present.");
+		}
+		repository.save(t);
+	}
+
+}
---
+++ case/src/main/java/com/effectiv/crm/domain/Case.java
@@ -0,0 +1,69 @@
+package com.effectiv.crm.domain;
+
+import java.util.Date;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="T_CASE")
+class Case extends BaseEntity{
+
+
+	@Column(name="SUMMARY", nullable=false)
+	private String summary;
+
+	@Column(name="TITLE", nullable=false)
+	private String title;
+	
+	@Column(name="CASE_STATUS_ID")
+	private String statusId;
+	
+	@Column(name="PRIORITY_ID")
+	private String prority;
+	
+	@Column(name="CONTACT_ID")
+	private String contactId;
+	
+	@Column(name="ACCOUNT_ID")
+	private String accountId;
+	
+	@Column(name="PRODUCT_ID")
+	private String productId;
+	
+	@Column(name="SLA_ID")
+	private String sla;
+	
+	@Column(name="CHANNEL_ID")
+	private String channelId;
+	
+	@Column(name="ASSIGNED_USER_ID", nullable=true)
+	private String assignedUser;
+	
+	@Column(name="ASSIGNED_TEAM_ID", nullable=true)
+	private String assignedTeam;
+	
+	//@OneToOne(optional=false,fetch = FetchType.EAGER)
+	//Group assignedGroup
+	
+	@Column(name="RESOLUTION", nullable=true)
+	private String resolution;
+	
+	@Column(name="PRIMARY_EMAIL", nullable=true)
+	private String primaryEmail;
+	
+	@Column(name="CATEGORY_ID")
+	private String categoryId;
+	
+	@Column(name="SUB_CATEGORY_ID")
+	private String subCategoryId;
+	
+	@Column(name="RESOLUTION_TYPE_ID")
+	private String  resolutionTypeId;
+	
+	@Column(name="DEFFERED_DATE")
+	private Date defferedDate;
+	
+	@Column(name="CLOSED_DATE")
+	private Date closedDate;
+	
+}
