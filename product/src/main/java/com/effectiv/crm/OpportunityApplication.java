366e34bef5d7224bc5702c1503d9f1eccb0b21ab
Dhrubo <dhrubo.kayal@gmail.com>
2017-05-02 00:01:48 -0500

    Product service

--- product/src/main/java/com/effectiv/crm/OpportunityApplication.java
+++ product/src/main/java/com/effectiv/crm/OpportunityApplication.java
@@ -2,13 +2,11 @@ package com.effectiv.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport
-public class LeadApplication {
+public class OpportunityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LeadApplication.class, args);
+		SpringApplication.run(OpportunityApplication.class, args);
 	}
 }
