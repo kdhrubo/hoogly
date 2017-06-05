0bc202dab0ba82b792c69a9e8a8cb8aa3e078825
Rajiv-mar121 <rajiv.kumar.102@gmail.com>
2017-06-06 00:43:44 +0530

    #15

--- lead/src/test/java/com/effectiv/crm/controller/ft/LeadControllerIntegrationTests.java
+++ lead/src/test/java/com/effectiv/crm/controller/ft/LeadControllerIntegrationTests.java
@@ -1,24 +1,46 @@
 package com.effectiv.crm.controller.ft;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
 
-
+import com.effectiv.crm.business.LeadBusinessDelegate;
+import com.effectiv.crm.controller.LeadController;
 import com.effectiv.crm.domain.Lead;
+import com.effectiv.crm.repository.LeadRepository;
+import com.effectiv.crm.web.SearchRequest;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 
@@ -26,7 +48,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
-//@Transactional
+@Transactional
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
 	DbUnitTestExecutionListener.class })
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
@@ -37,6 +59,20 @@ public class LeadControllerIntegrationTests {
 	private TestRestTemplate restTemplate;
 
 	private final String BASE_URL = "/leads";
+	
+	@MockBean
+	private LeadBusinessDelegate leadBusinessDelegate;
+	
+	@Autowired
+	private LeadRepository leadRepository;
+	private LeadController leadController;
+	
+	@Before
+	public void setUp() {
+		leadController =Mockito.mock(LeadController.class);
+		leadRepository = Mockito.mock(LeadRepository.class);
+		leadBusinessDelegate = new LeadBusinessDelegate(leadRepository);
+	}
 
 	@Test
 	public void create() {
@@ -66,6 +102,8 @@ public class LeadControllerIntegrationTests {
 	
 	public void findOne() {
 		ResponseEntity<Lead> responseEntity = restTemplate.getForEntity(BASE_URL + "/1", Lead.class);
+		log.info("retrieved responseEntity- {}", responseEntity);
+		log.info("retrieved getBody- {}", responseEntity.getBody());
 		Lead retrievedLead = responseEntity.getBody();
 		log.info("retrieved lead - {}", retrievedLead);
 		assertThat(HttpStatus.FOUND, equalTo( responseEntity.getStatusCode()));
@@ -76,6 +114,7 @@ public class LeadControllerIntegrationTests {
 	//@Test
 	public void update() {
 		// fail("Not implemented");
+	//	when(leadRepository.findOne(any(String.class))).thenReturn(lead);
 	}
 
 	//@Test
@@ -98,9 +137,36 @@ public class LeadControllerIntegrationTests {
 		// fail("Not implemented");
 	}
 
-	//@Test
+	@Test
 	public void findAll() {
-		// fail("Not implemented");
+		List<Lead> leadLists=new ArrayList<Lead>();
+		Lead lead=new Lead();
+		int id=1;
+		for(int i=0;i<10;i++)
+		{
+			lead.setId(Integer.toString(id));
+			leadLists.add(lead);
+			id +=1;
+			lead=new Lead();
+			
+		}
+		when(leadBusinessDelegate.findAll(any(SearchRequest.class), any(Pageable.class))).thenReturn(new PageImpl<>(leadLists));
+		SearchRequest sr = new SearchRequest();
+		PageRequest pr = new PageRequest(1, 5);
+		
+		Page<Lead> rLeadPage = leadBusinessDelegate.findAll(sr, pr);
+		assertThat(rLeadPage.getTotalElements()).isEqualTo(10L);
+		assertThat(rLeadPage.getTotalPages()).isEqualTo(1);
+		
+		List<Lead> rLeads = rLeadPage.getContent();
+		int counter = 1;
+		for (Lead ld : rLeads) {
+			assertThat(ld.getId()).isEqualTo(counter + "");
+			counter++;
+		}
+		
+		verify(leadRepository, times(1)).findAll(sr,pr);
+		verifyNoMoreInteractions(leadRepository);
 	}
 
 	//@Test
